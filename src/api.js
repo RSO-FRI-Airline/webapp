54263d7979410e96c2c50bbafa8cb3b8c0d4abc9
Jaka Kordez <jaka.kordez@gmail.com>
2019-01-09 08:15:17 +0100

    Updated backend services URLs

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export default{
-    bookingPort: "30144",
-    searchPort: "30599",
+    searchPort: "35.233.177.20:8081",
+    bookingPort: "35.247.27.154:8082",
     get: function (service, endpoint){
         var port = this.searchPort;
         if(service == "booking"){
             port = this.bookingPort;
         }
-        return axios.get(`http://159.122.183.250:${port}/v1/${endpoint}`, 
+        return axios.get(`http://${port}/v1/${endpoint}`, 
             { headers: { 'crossDomain': true, 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}});
     },
     post: function (service, endpoint, data){
@@ -16,7 +16,7 @@ export default{
         if(service == "booking"){
             port = this.bookingPort;
         }
-        return axios.post(`http://159.122.183.250:${port}/v1/${endpoint}`, data, 
+        return axios.post(`http://${port}/v1/${endpoint}`, data, 
             { headers: { 'crossDomain': true, 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}});
     }
 };
